3bc50f2bec5cd8ba6c01610e03dad6fed5dfb0af
Sofia Mudry <sofiamudry@MBP-de-Sofi.fibertel.com.ar>
2022-04-16 12:05:18 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,12 @@
+console.log('direccion');
+
+alert('Hola, Bienvenide!');
+
+const calle = prompt('Ingrese su calle');
+const numero = prompt('Ingrese su numero');
+const departamento = prompt('Ingrese su departamento');
+const codigoPostal = prompt('Ingrese su codigo postal');
+const ciudad = prompt('Ingrese su ciudad');
+const pais = prompt('Ingrese su pais');
+
+alert(`La direccion que usted a ingresado es ${calle} ${numero}, ${departamento}. ${codigoPostal}. ${ciudad}, ${pais}.`);
